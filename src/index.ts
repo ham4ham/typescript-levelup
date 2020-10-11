99ea2d42164d3f9e1f01aad4268e5428f6e911ff
ham4ham <72608594+ham4ham@users.noreply.github.com>
2020-10-11 16:52:48 -0400

    fastify server running (#7)
    Co-authored-by: jwin4740@gmail.com <jwin4740@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.warn('hello world');
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const server = Fastify();
+const port = Number(process.env.PORT) || 3000;
+
+server.listen(port, '0.0.0.0', error => {
+  if (error) {
+    server.log.error('Unable to start fastify server');
+    process.exit(1);
+  }
+  const startMsg = `...server started on port ${port} on ${new Date().toDateString()}`;
+  console.log(startMsg);
+  server.log.info(startMsg);
+});
